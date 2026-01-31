9ee1ac5ab2a736cfe3c5903fd08b1bcb7d6a359b
Kushuh <geoffroy.vincent@agorastoryverse.com>
2026-02-01 00:32:11 +0100

    hotfix(logging): colors

--- logging/presets/utils.go
+++ logging/presets/utils.go
@@ -3,7 +3,7 @@ package loggingpresets
 import "github.com/charmbracelet/lipgloss"
 
 var (
-	LipColorError = lipgloss.Color("#e6203e")
+	LipColorError = lipgloss.Color("#ff3232")
 	LipColorWarn  = lipgloss.Color("#ffb600")
 	LipColorInfo  = lipgloss.Color("#25e6e3")
 )
